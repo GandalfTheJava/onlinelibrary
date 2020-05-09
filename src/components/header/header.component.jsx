3be2907de0b80d62f50757c9cdc357cb9129b9cb
mehmet <mehmetcimen23@gmail.com>
2020-05-09 14:45:54 +0100

    Minor updates

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../../Global.scss";
+import logo from '../../assets/logo.svg';
+
+function Header() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Header;
