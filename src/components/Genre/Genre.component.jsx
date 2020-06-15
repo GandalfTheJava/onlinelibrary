8047de0e0a6c9fd3aea6b0b3e41796f4e442a04b
mehmet <mehmetcimen23@gmail.com>
2020-06-15 10:57:08 +0100

    Updates

--- src/components/Genre/Genre.component.jsx
+++ src/components/Genre/Genre.component.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import DirectoryItem from '../directory-item/directory-item.component';
+import { directoryContext } from '../provider/directory.provider';
 
-function Genre({ GenreName }) {
+function Genre({ match }) {
+    const { params: { genreid } } = match;
+    const { directory } = useContext(directoryContext);
+
     return (
         <div>
-            {GenreName}
+            {
+                genreid
+            }
+
         </div>
     )
 }
