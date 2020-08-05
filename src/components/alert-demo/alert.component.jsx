a22c6f5a885ddd45b3bac4bfa7c671c9661fa25f
mehmet <mehmetcimen23@gmail.com>
2020-08-05 12:24:27 +0100

    updates

--- src/components/alert-demo/alert.component.jsx
+++ src/components/alert-demo/alert.component.jsx
@@ -4,7 +4,7 @@ function AlertDemo() {
     return (
         <div>
             <Alert variant="success">
-                <Alert.Heading>Hey, Thank you for checking out my project.</Alert.Heading>
+                <Alert.Heading>Hello, Thank you for checking out my project.</Alert.Heading>
                 <p>
                     Since this project is still in development, there may be slight issues/bugs which may affect performance,
                     and or the overall feel of the application.
