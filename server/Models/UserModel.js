f4bd74397c91c367a37a4faa96825ae0823249a3
mehmet <mehmetcimen23@gmail.com>
2020-08-07 18:48:49 +0100

    Authenticate requests on the /users/register endpoint

--- server/Models/UserModel.js
+++ server/Models/UserModel.js
@@ -9,6 +9,9 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         minlength: 6
+    },
+    displayName: {
+        type: String
     }
 });
 
--- server/Routes/UserRoute.js
+++ server/Routes/UserRoute.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
 
-router.get("/test", (req, res) => {
-    res.send("Yeah nice")
-})
+router.post("/register", async (req, res) => {
+    const { email, password, passwordCheck, displayName } = req.body;
+
+    if (!email || !password || !passwordCheck) {
+        return res.status(400).json({ message: "Please Complete All Fields" });
+    }
+
+    if (password.length < 6) {
+        return res.status(400).json({ message: "Password must be at least 6 characters long" });
+    }
+
+    if (password != passwordCheck) {
+        return res.status(400).json({ message: "Passwords do not match" });
+    }
+
+    res.send("Nice");
+});
 
 module.exports = router;
